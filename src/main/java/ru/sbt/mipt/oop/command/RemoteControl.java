c05fca9eeffaf17611a378723618d26d55fc0051
sanshous <63168235+sanshous@users.noreply.github.com>
2021-05-13 21:09:20 +0300

    Alarm

--- src/main/java/ru/sbt/mipt/oop/command/RemoteControl.java
+++ src/main/java/ru/sbt/mipt/oop/command/RemoteControl.java
@@ -9,13 +9,8 @@ import java.util.HashMap;
 public class RemoteControl implements rc.RemoteControl {
     private HashMap<String, Command> controlToButton;
 
-    public void createControl(SmartHome smartHome, String[] buttons) {
-        controlToButton.put(buttons[0], new AlarmActivating(smartHome));
-        controlToButton.put(buttons[1], new AlarmAlerting(smartHome));
-        controlToButton.put(buttons[2], new DoorClosedCommand(smartHome));
-        controlToButton.put(buttons[3], new HallLightTurnedOn(smartHome));
-        controlToButton.put(buttons[4], new LightTurnedOn(smartHome));
-        controlToButton.put(buttons[5], new TurnOffLightCommand(smartHome));
+    public void createControl(HashMap<String, Command> controlToButton) {
+        this.controlToButton = controlToButton;
     }
 
     @Override
